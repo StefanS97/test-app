37264fdf582b7990d587d18b59ab167300d1fe90
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-14 19:49:30 +0200

    Added Role Field

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,18 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+                            <div class="col-md-6">
+                                <input id="role" type="text" class="form-control @error('email') is-invalid @enderror" name="role" value="{{ old('role') }}" required>
+                                @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">

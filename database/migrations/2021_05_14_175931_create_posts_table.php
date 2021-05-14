f0e4cb0ac9effb0954dacf6be2d07f06d15ffcfe
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-14 21:36:09 +0200

    without comments

--- database/migrations/2021_05_14_175931_create_posts_table.php
+++ database/migrations/2021_05_14_175931_create_posts_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreatePostsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
@@ -21,12 +16,7 @@ class CreatePostsTable extends Migration
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    
     public function down()
     {
         Schema::dropIfExists('posts');

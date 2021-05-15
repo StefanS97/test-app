bcd26f45e70a6cdfcee04030fae15f113b0f77e5
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-15 16:18:31 +0200

    bug

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,7 +34,7 @@ class PostController extends Controller
 
     public function show(Post $post)
     {
-        return view('post.post', ['post' => $post]);
+        return view('post.show', ['post' => $post]);
     }
 
     public function edit(Post $post)
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,13 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-
-        @foreach ($posts as $post)
-        
         <x-post/>
-        
-        @endforeach
-        
     </div>
 </div>
 @endsection

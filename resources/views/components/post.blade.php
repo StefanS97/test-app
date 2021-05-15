7650fe7def2c7caff91daaaeeeaa29d17ce625c3
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-15 16:05:06 +0200

    post component

---
+++ resources/views/components/post.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="card m-2" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">{{$slot->name}}</h5>
+                <p class="card-text">{{$slot->text}}</p>
+                <p>By: {{$slot->user->name}}</p>
+                <a href="/post/{{$slot->id}}" class="btn btn-primary">View Post</a>
+            </div>
+        </div>
+    </div>
+</div>

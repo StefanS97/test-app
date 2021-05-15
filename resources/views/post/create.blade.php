572884e044f18ae7c869d4c76f45e6435669b864
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-15 19:51:33 +0200

    asd

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -13,11 +13,17 @@
                         <input id="postName" name="postName" class="form-control @error('postName') is-invalid @enderror"
                                type="text" placeholder="Post Name" value="{{ old('postName') }}">
                     </div>
+                    @error('postName')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                     <div class="form-group">
                         <label for="postText">Text</label>
                         <input id="postText" name="postText" class="form-control @error('postText') is-invalid @enderror"
                                type="text" placeholder="Text" value="{{ old('postText') }}">
                     </div>
+                    @error('postText')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>

197bda54aaceeef1d399e847a052f0d1c6bef511
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-15 16:32:31 +0200

    fixed

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        @foreach ($posts as $post)
         <div class="col-6">
-            @foreach ($posts as $post)
             
             <x-post :post='$post'/>
             
-            @endforeach
         </div>
+            @endforeach
     </div>
 </div>
 @endsection

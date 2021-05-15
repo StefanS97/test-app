d272b9bf2e942f6eb9343bc468fad1b7edc1780e
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-15 18:25:14 +0200

    laravel has mixed feelings

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,12 +5,12 @@
     <div class="row justify-content-center">
         <form>
             <div class="form-group">
-              <label for="name">Name</label>
-              <input type="text" class="form-control" id="name" aria-describedby="name" placeholder="name">
+              <label for="PostName">Name of Post</label>
+              <input type="text" class="form-control" id="postname" aria-describedby="postname" placeholder="Post Name">
             </div>
             <div class="form-group">
-              <label for="text">text</label>
-              <input type="text" class="form-control" id="text" placeholder="text">
+              <label for="text">Text</label>
+              <input type="text" class="form-control" id="text" placeholder="Text">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
           </form>
--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,9 @@ Route::get('/post', [PostController::class, 'index'])
     ->middleware('auth')
     ->name('posts.index');
 
-Route::get('/post/{post}', [PostController::class, 'show'])
-    ->middleware('auth')
-    ->name('posts.show');
+// Route::get('/post/{post}', [PostController::class, 'show'])
+//     ->middleware('auth')
+//     ->name('posts.show');
 
 Route::get('/post/create', [PostController::class, 'create'])
     ->middleware('auth')

d05583b384f973bdd76f205e98dde71cef8088e5
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-15 19:06:28 +0200

    asd

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,7 +29,7 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
-        ddd('here');
+        ddd($request);
     }
 
     public function show(Post $post)

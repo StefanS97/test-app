9c17dc9106afa99cec13463e5d965ea87be8775e
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-15 18:39:53 +0200

    col-6 added

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -3,17 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <form>
-            <div class="form-group">
-                <label for="PostName">Post Name</label>
-                <input type="text" class="form-control" id="postname" aria-describedby="postname" placeholder="Post Name">
-            </div>
-            <div class="form-group">
-                <label for="text">Text</label>
-                <input type="text" class="form-control" id="text" placeholder="Text">
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        <div class="col-6">
+            <form>
+                <div class="form-group">
+                    <label for="PostName">Post Name</label>
+                    <input type="text" class="form-control" id="postname" aria-describedby="postname" placeholder="Post Name">
+                </div>
+                <div class="form-group">
+                    <label for="text">Text</label>
+                    <input type="text" class="form-control" id="text" placeholder="Text">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
     </div>
 </div>
 @endsection

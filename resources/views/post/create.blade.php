d94d66421746396d70326131618a2e6d17e5daaa
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-15 18:52:51 +0200

    changes done

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -9,11 +9,13 @@
                 <form class="p-3">
                     <div class="form-group">
                         <label for="postName">Post Name</label>
-                        <input type="text" class="form-control" id="postName" placeholder="Post Name">
+                        <input id="postName" name="postName" class="form-control" 
+                        type="text" placeholder="Post Name">
                     </div>
                     <div class="form-group">
                         <label for="postText">Text</label>
-                        <input type="text" class="form-control" id="postText" placeholder="Text">
+                        <input id="postText"" name="postText" class="form-control" 
+                        type="text" placeholder="Text">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>

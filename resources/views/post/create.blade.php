f4fad5f8125e99848a5a3505144e4026d46c284e
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-15 18:37:26 +0200

    asd

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,15 +5,15 @@
     <div class="row justify-content-center">
         <form>
             <div class="form-group">
-              <label for="PostName">Post Name</label>
-              <input type="text" class="form-control" id="postname" aria-describedby="postname" placeholder="Post Name">
+                <label for="PostName">Post Name</label>
+                <input type="text" class="form-control" id="postname" aria-describedby="postname" placeholder="Post Name">
             </div>
             <div class="form-group">
-              <label for="text">Text</label>
-              <input type="text" class="form-control" id="text" placeholder="Text">
+                <label for="text">Text</label>
+                <input type="text" class="form-control" id="text" placeholder="Text">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+        </form>
     </div>
 </div>
 @endsection

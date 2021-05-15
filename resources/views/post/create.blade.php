b089a4cff1e018d3ba8c8dc89c0f65e29ac5e8bf
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-15 18:42:38 +0200

    acrd

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -4,17 +4,19 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-6">
-            <form>
-                <div class="form-group">
-                    <label for="PostName">Post Name</label>
-                    <input type="text" class="form-control" id="postname" aria-describedby="postname" placeholder="Post Name">
-                </div>
-                <div class="form-group">
-                    <label for="text">Text</label>
-                    <input type="text" class="form-control" id="text" placeholder="Text">
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            <div class="card p-3">
+                <form>
+                    <div class="form-group">
+                        <label for="PostName">Post Name</label>
+                        <input type="text" class="form-control" id="postname" aria-describedby="postname" placeholder="Post Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="text">Text</label>
+                        <input type="text" class="form-control" id="text" placeholder="Text">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>

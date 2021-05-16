762a165e72e9ba7c936d731a6704d426fb8c8912
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-16 22:57:25 +0200

    pr-4

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -8,7 +8,7 @@
                 class="btn btn-primary">View Post</a>
         @elseif ($type === 'show')
             <a href="{{ route('posts.edit', ['post' => $post->id]) }}" class="btn btn-success">Edit</a>
-            <form class="d-inline" action="{{ route('posts.delete', ['post' => $post->id]) }}" method="POST">
+            <form class="d-inline pr-4 " action="{{ route('posts.delete', ['post' => $post->id]) }}" method="POST">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="btn btn-danger">Delete</button>

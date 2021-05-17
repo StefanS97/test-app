2327c17b8da0acf47f96d356a2203f570f5263a3
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-17 12:20:21 +0200

    button

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -8,11 +8,13 @@
                 class="btn btn-primary">View Post</a>
         @elseif ($type === 'show')
             <a href="{{ route('posts.edit', ['post' => $post->id]) }}" class="btn btn-success">Edit</a>
-            <form class="d-inline pr-4 " action="{{ route('posts.delete', ['post' => $post->id]) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">Delete</button>
-            </form>
+            @can('delete', $post)
+                <form class="d-inline pr-4 " action="{{ route('posts.delete', ['post' => $post->id]) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            @endcan
             <a href="{{ route('posts.index') }}" class="btn btn-primary">Back</a>
         @endif
     </div>

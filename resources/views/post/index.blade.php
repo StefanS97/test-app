976b52861ee3518fd73b8ba28e8f983581d3f449
stefan.stojkov97@gmail.com <stefan.stojkov97@gmail.com>
2021-05-31 12:04:48 +0200

    fixed responsivness

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid" style="max-width: 1440px;">
     <div class="row justify-content-center">
         <div class="col-md-4 my-2">
             <div>
